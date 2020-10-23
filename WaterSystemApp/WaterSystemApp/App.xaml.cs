75c29f9e97815eacb8978d034a75ae8dbfb5b62c
L10Messi10 <loveyourenemy@outlook.com>
2020-10-23 13:21:24 +0800

    Add project files.

---
+++ WaterSystemApp/WaterSystemApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WaterSystemApp.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WaterSystemApp
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ WaterSystemApp/WaterSystemApp/View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace WaterSystemApp.View
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage
+    {
+        public MainPage() => InitializeComponent();
+    }
+}
